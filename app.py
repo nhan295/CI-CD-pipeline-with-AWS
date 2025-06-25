0a2b918137418defb515eb47422cd4326eaf9427
nhan295 <tranthihongnhan295@gmail.com>
2025-06-25 16:55:07 +0700

    refactor: update hello route to render profile template and add debug mode

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'HELLO WORLD!!'
+    return render_template('profile.html',name='iamnhan')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000,debug=True)
     
